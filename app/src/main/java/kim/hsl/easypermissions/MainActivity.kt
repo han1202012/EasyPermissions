ebbddcc99767b12a88ad7ca524ed80d47b9c14e7
han1202012 <octopus_truth@163.com>
2020-09-18 22:11:25 +0800

    update

--- app/src/main/java/kim/hsl/easypermissions/MainActivity.kt
+++ app/src/main/java/kim/hsl/easypermissions/MainActivity.kt
@@ -1,11 +1,14 @@
 package kim.hsl.easypermissions
 
 import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 import pub.devrel.easypermissions.EasyPermissions.PermissionCallbacks
 import pub.devrel.easypermissions.EasyPermissions.RationaleCallbacks
@@ -56,6 +59,18 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks, RationaleCallback
      */
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
         Log.i(TAG, "用户拒绝权限申请 , 请求码 $requestCode , 拒绝的权限 : $perms")
+
+
+        // 如果申请的权限中有任何一个权限存在 永久拒绝 的情况
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            // 设置 引导用户前往设置界面 自行设置权限的引导对话框
+            AppSettingsDialog.Builder(this)
+                .setTitle("需要手动设置权限")
+                .setRationale("存在永久拒绝的权限 , 需要手动前往设置界面为应用进行授权")
+                .setPositiveButton("前往设置界面")
+                .setNegativeButton("不使用该功能")
+                .build().show()
+        }
     }
 
     /**
@@ -69,7 +84,6 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks, RationaleCallback
 
 
 
-
     /*
         三 、 实现 EasyPermissions.RationaleCallbacks 接口中的方法
      */
@@ -79,7 +93,7 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks, RationaleCallback
      *
      */
     override fun onRationaleDenied(requestCode: Int) {
-
+        Log.i(TAG, "权限申请原理对话框中选择 取消 , 请求码 $requestCode")
     }
 
     /**
@@ -87,7 +101,7 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks, RationaleCallback
      *
      */
     override fun onRationaleAccepted(requestCode: Int) {
-
+        Log.i(TAG, "权限申请原理对话框中选择 确定 , 请求码 $requestCode")
     }
 
 
@@ -110,8 +124,10 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks, RationaleCallback
     fun doSomethingWithPermissions(){
 
         // 数组前加上 * 符号 , 可以将数组展开 , 转为可变数组
+        // 调用 EasyPermissions.hasPermissions 方法判定是否已经申请该权限
         if(EasyPermissions.hasPermissions(this, *PERMMISSIONS)){
             // 如果有上述权限, 执行该操作
+            Toast.makeText(this, "权限申请通过", Toast.LENGTH_LONG).show()
         }else{
             // 如果没有上述权限 , 那么申请权限
             EasyPermissions.requestPermissions(
@@ -122,6 +138,23 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks, RationaleCallback
                 // 数组前加上 * 符号 , 可以将数组展开 , 转为可变数组
                 *PERMMISSIONS
             )
+        }
+    }
+
+
+    /**
+     * 从 AppSettingsDialog 界面中返回, 回调该方法
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE){
+            // 判断五种权限是否申请成功
+            var hasPermissions =
+                EasyPermissions.hasPermissions(this, *PERMMISSIONS)
+            // 界面中显示权限申请结果
+            Toast.makeText(this, "设置界面用户手动申请权限结果 $hasPermissions",
+                Toast.LENGTH_LONG).show()
         }
     }
 
